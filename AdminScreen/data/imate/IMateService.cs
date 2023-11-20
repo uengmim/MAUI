d3f259c38435671eb403e91f054dcbaa8eb2b409
채승민 <yalmipzzi@naver.com>
2023-11-20 09:58:35 +0900

    20231120 imateservice 추가

---
+++ AdminScreen/data/imate/IMateService.cs
@@ -0,0 +1,11 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+
+//namespace AdminScreen.data.imate
+//{
+//    public interface IMateService
+//    {
+//        Task<Result<User>> UserCheck(string id);
+//    }
+//}
