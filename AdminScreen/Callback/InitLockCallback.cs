46f9cb7d6688e8b46e649a5e6160abc93ea0ecae
채승민 <yalmipzzi@naver.com>
2023-11-30 09:20:11 +0900

    20231130 callback

---
+++ AdminScreen/Callback/InitLockCallback.cs
@@ -0,0 +1,26 @@
+﻿
+using AdminScreen.ViewModels;
+using AndroidX.Lifecycle;
+
+namespace AdminScreen
+{
+    /// <summary>
+    /// Lock Scan Callback
+    /// </summary>
+    public partial class InitLockCallback
+    {
+        /// <summary>
+        /// View 모델
+        /// </summary>
+        public LockRegigterModel ViewModel { get; set; }
+
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        /// <param name="viewModel">뷰모델</param>
+        public InitLockCallback(LockRegigterModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+    }
+}
