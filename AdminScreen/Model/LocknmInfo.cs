d15671f30e06a02c54904c8dc8c579cae4c9b1b7
채승민 <yalmipzzi@naver.com>
2023-11-30 09:44:30 +0900

    model

---
+++ AdminScreen/Model/LocknmInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace AdminScreen.Model
+{
+    public class LocknmInfo
+    {
+        /// <summary>
+        /// 자물쇠 이름
+        /// </summary>
+        public string Locknm { get; set; }
+    }
+}
