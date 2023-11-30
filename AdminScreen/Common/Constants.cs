1e09134ee5b5d3058cae182e0040e3564876e40b
채승민 <yalmipzzi@naver.com>
2023-11-30 09:20:36 +0900

    20231130 constants

---
+++ AdminScreen/Common/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace AdminScreen.Common
+{
+    public class Constants
+    {
+
+        public static string SaveIDKey = "SaveID";
+
+        public static string SavePWKey = "SavePW";
+
+    }
+}
