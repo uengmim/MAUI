6a481a14ba83ccc4743b9a74f3f8bd8f052113eb
채승민 <yalmipzzi@naver.com>
2023-12-04 10:27:35 +0900

    20231204 모델 추가

---
+++ AdminScreen/Platforms/Android/Model/LockInfo.cs
@@ -0,0 +1,27 @@
+﻿using Com.Ttlock.BL.Sdk.Api;
+
+namespace AdminScreen.Models
+{
+    /// <summary>
+    /// LOCK ITEM
+    /// </summary>
+    public partial class LockInfo
+    {
+        /// <summary>
+        /// Device 정보
+        /// </summary>
+        public ExtendedBluetoothDevice Device { get; private set; }
+
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        /// <param name="device">TTlock Device 정보</param>
+        public LockInfo(ExtendedBluetoothDevice device)
+        {
+            Device = device;
+            
+            LockName = Device.Name;
+            LockMac = Device.Address;
+        }
+    }
+}
