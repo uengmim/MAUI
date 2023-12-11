61aa31c8824182432e676c5f637ec65deab92bab
채승민 <yalmipzzi@naver.com>
2023-12-11 09:22:04 +0900

    20231211 lock 등록

---
+++ AdminScreen/Platforms/Android/Callback/RegistSearchScanCallback.Android.cs
@@ -0,0 +1,26 @@
+﻿using Com.Ttlock.BL.Sdk.Api;
+using Com.Ttlock.BL.Sdk.Callback;
+using Com.Ttlock.BL.Sdk.Entity;
+
+namespace AdminScreen
+{
+    /// <summary>
+    /// Lock Scan Callback
+    /// </summary>
+    public partial class RegistSearchScanCallback : Java.Lang.Object, IScanLockCallback
+    {
+        public void OnFail(LockError p0)
+        {
+            Console.WriteLine($"{p0.Lockmac}, {p0.Description}");
+        }
+
+        public void OnScanLockSuccess(ExtendedBluetoothDevice p0)
+        {
+            //p0.Device
+            if (!ViewModel.LockDataModel.Any(l => l.LockMac == p0.Address))
+                ViewModel.LockDataModel.Add(new Models.LockInfo(p0));
+
+            Console.WriteLine(p0.Address);
+        }
+    }
+}
