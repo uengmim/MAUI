201cb82fd2b4f279452431cca4f0b7f3effa191b
채승민 <yalmipzzi@naver.com>
2023-12-13 09:07:07 +0900

    20231213 model

---
+++ AdminScreen/Model/EmpMstItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+
+namespace AdminScreen.Models
+{
+    public class EmpMstItem : INotifyPropertyChanged
+    {
+
+        private string id;
+        private string name;
+        /// <summary>
+        /// 작업자
+        /// </summary>
+        public string ID { get { return id; } set { id = value; OnPropertyChanged(nameof(ID)); } }
+        public string Name { get { return name; } set { name = value; OnPropertyChanged(nameof(Name)); } }
+
+
+        public EmpMstItem(string id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, args);
+        }
+
+    }
+}
