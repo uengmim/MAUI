02ff3c655aceea5bdeecb7c5b127171f0c1f8e56
채승민 <yalmipzzi@naver.com>
2023-12-13 09:07:54 +0900

    20231213 고객 로그인

---
+++ ClientScreen/Model/ClientLogInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ClientScreen.Model
+{
+    public class ClientLogInfo
+    {
+        /// <summary>
+        /// 로그인 아이디
+        /// </summary>
+        public string CliID { get; set; }
+
+        /// <summary>
+        /// 로그인 패스워드
+        /// </summary>
+        public string CliPW { get; set; }
+    }
+}
