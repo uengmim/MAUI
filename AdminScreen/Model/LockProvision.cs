cb219c47b3a97786ba175631950898da5811dc60
채승민 <yalmipzzi@naver.com>
2023-12-15 09:23:26 +0900

    20231215 지급 모델

---
+++ AdminScreen/Model/LockProvision.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+
+namespace AdminScreen.Models
+{
+    public class LockProvision : INotifyPropertyChanged
+    {
+
+        private string lsn;
+        private string lknm;
+
+        /// <summary>
+        /// 로그인 아이디
+        /// </summary>
+        public string LSN { get { return lsn; } set { lsn = value; OnPropertyChanged(nameof(LSN)); } }
+        public string LKNM { get { return lknm; } set { lknm = value; OnPropertyChanged(nameof(LKNM)); } }
+
+
+        public LockProvision(string lsn, string lknm)
+        {
+            LSN = lsn;
+            LKNM = lknm;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, args);
+        }
+
+    }
+}
