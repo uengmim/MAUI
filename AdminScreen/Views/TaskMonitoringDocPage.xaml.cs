07989020d10b68ced67f24900bdea3bed3250d86
채승민 <yalmipzzi@naver.com>
2023-12-18 10:51:58 +0900

    20231218 작업 조회

---
+++ AdminScreen/Views/TaskMonitoringDocPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace AdminScreen.Views
+{
+    public partial class TaskMonitoringDocPage : ContentPage
+    {
+
+        public TaskMonitoringDocPage()
+        {
+            InitializeComponent();
+        }
+
+
+        public async void LockPictureClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DocLockPicturePage());
+        }
+
+        public async void CrushingReportClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DocCrushingReportPage());
+        }
+        public async void CrushingProofClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DocCrushingProofPage());
+        }
+    }
+}
