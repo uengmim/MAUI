9caa8f6370bb1cbdbeca3464657ae0dd197b732a
채승민 <yalmipzzi@naver.com>
2023-12-18 10:51:08 +0900

    20231218 관리자 모델

---
+++ AdminScreen/Model/LockSearchData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+
+namespace AdminScreen.Models
+{
+    public class LockSearchData : INotifyPropertyChanged
+    {
+
+        private string lsn;
+        private string lknm;
+        private string mac;
+
+
+
+        public string LSN { get { return lsn; } set { lsn = value; OnPropertyChanged(nameof(LSN)); } }
+        public string LKNM { get { return lknm; } set { lknm = value; OnPropertyChanged(nameof(LKNM)); } }
+        public string MAC { get { return mac; } set { mac = value; OnPropertyChanged(nameof(MAC)); } }
+
+
+
+        public LockSearchData(string lsn, string lknm, string mac)
+        {
+            LSN = lsn;
+            LKNM = lknm;
+            MAC = mac;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, args);
+        }
+    }
+}
