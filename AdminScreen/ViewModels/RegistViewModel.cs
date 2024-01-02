11168ec6b1f6c7e87d6b6557fdddfc5594cf5f5a
채승민 <yalmipzzi@naver.com>
2024-01-02 09:04:24 +0900

    20240102 admin 등록

---
+++ AdminScreen/ViewModels/RegistViewModel.cs
@@ -0,0 +1,32 @@
+﻿using AdminScreen.Common;
+using AdminScreen.Models;
+using AdminScreen.Views;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+
+namespace AdminScreen.ViewModel
+{
+    public class RecoveryViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Commands
+        public INavigation Navigation { get; set; }
+        public ICommand RecoveryDetailCommand => new Command(RecoveryDetailSecurity);
+        #endregion
+        // Methods
+        #region Navigation
+        private async void RecoveryDetailSecurity()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new RecoverySearchPage());
+        }
+        #endregion
+
+    }
+}
