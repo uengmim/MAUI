c26384647cfb4602bf08c8f47588c558a67f536b
채승민 <yalmipzzi@naver.com>
2024-01-08 09:37:13 +0900

    20240108 고객 로그인

---
+++ ClientScreen/Model/ClientLoginInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ClientScreen.Model
+{
+    public class ClientLoginInfo
+    {
+        //로그인 아이디
+        public string ClientID { get; set; }
+        public string ClientPW { get; set; }
+        public string ClientName{ get; set; }
+        public string EMPNO { get; set; }
+        public string DEPTID { get; set; }
+        public string ClientDep { get; set; }
+
+    }
+}
