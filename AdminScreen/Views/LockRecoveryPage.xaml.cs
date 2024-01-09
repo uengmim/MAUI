a447e31e4cb66657c00d843e856c28653e6aa0d4
채승민 <yalmipzzi@naver.com>
2024-01-09 09:27:06 +0900

    20240109 관리자 회수

---
+++ AdminScreen/Views/LockRecoveryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AdminScreen.Views
+{
+    public partial class LockRecoveryPage : ContentPage
+    {
+        public LockRecoveryPage()
+        {
+            InitializeComponent();
+
+        }
+        public async void RecoveryClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RecoverySearchPage());
+        }
+    } 
+}
