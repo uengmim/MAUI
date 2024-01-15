9a8f3396b3457ea4b44c4782004b7f560ecd706d
채승민 <yalmipzzi@naver.com>
2024-01-15 09:45:55 +0900

    20240115 관리자 메인

--- AdminScreen/Views/MainPage.xaml.cs
+++ AdminScreen/Views/MainPage.xaml.cs
@@ -9,10 +9,31 @@
             InitializeComponent();
         }
 
-        [Obsolete]
         public async void RegistClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new RegistPage());
+        }
+        public async void ProvisionClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LockProvisionPage());
+        }
+
+        public async void RecoveryClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LockRecoveryPage());
+        }
+        public async void MonitoringClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TaskMonitoringPage());
+        }
+        public async void HistoryCheckClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HistoryPage());
+        }
+        private async void Logout_Clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new LoginPage());
+
         }
     }
 }
