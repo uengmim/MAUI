91a8417a5c63f54d5d1d4ef9d104694fd9a57d4e
채승민 <yalmipzzi@naver.com>
2024-01-25 09:21:35 +0900

    20240125 고객 모델

---
+++ ClientScreen/Model/CliLockProvision.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+
+namespace ClientScreen.Models
+{
+    public class CliLockProvision : INotifyPropertyChanged
+    {
+
+        private string lsn;
+        private string lknm;
+
+        /// <summary>
+        /// 로그인 아이디
+        /// </summary>
+        public string LSN { get { return lsn; } set { lsn = value; OnPropertyChanged(nameof(LSN)); } }
+        public string LKNM { get { return lknm; } set { lknm = value; OnPropertyChanged(nameof(LKNM)); } }
+
+
+        public CliLockProvision(string lsn, string lknm)
+        {
+            LSN = lsn;
+            LKNM = lknm;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, args);
+        }
+
+    }
+}
