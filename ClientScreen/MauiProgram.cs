9bf90d5a428cbfec38506e06829d3b086b800245
채승민 <yalmipzzi@naver.com>
2024-01-25 09:22:41 +0900

    20240125

---
+++ ClientScreen/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+
+
+
+namespace ClientScreen
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseSkiaSharp(true)
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
