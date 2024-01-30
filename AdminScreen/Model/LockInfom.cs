9c25d55114df79a69d6af7f07eb4968777e99134
채승민 <yalmipzzi@naver.com>
2024-01-30 10:55:11 +0900

    20240130 관리자 모델

---
+++ AdminScreen/Model/LockInfom.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+
+namespace AdminScreen.Models
+{
+    public class LockInfom : INotifyPropertyChanged
+    {
+
+        private string lockdata;
+        private string lockname;
+
+        /// <summary>
+        /// 로그인 아이디
+        /// </summary>
+        public string LockData { get { return lockdata; } set { lockdata = value; OnPropertyChanged(nameof(LockData)); } }
+        public string LockName { get { return lockname; } set { lockname = value; OnPropertyChanged(nameof(LockName)); } }
+
+
+        public LockInfom(string lockdata, string lockname)
+        {
+            LockData = lockdata;
+            LockName = lockname;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, args);
+        }
+
+    }
+}
