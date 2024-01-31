9238435743438a888dcac77a43e518086a5187b9
채승민 <yalmipzzi@naver.com>
2024-01-31 10:01:11 +0900

    20240131 고객 모델

---
+++ ClientScreen/Model/ClientLogInDetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+
+namespace ClientScreen.Model
+{
+    public class ClientLogInDetail
+    {
+        /// <summary>
+        /// 전화번호
+        /// </summary>
+        public string DetailNumber { get; set; }
+
+        /// <summary>
+        /// 이름
+        /// </summary>
+        public string DetailName { get; set; }
+
+        /// <summary>
+        /// 사용할 E-MAIL
+        /// </summary>
+        public string DetailEmail { get; set; }
+
+        /// <summary>
+        /// 사용자 ID
+        /// </summary>
+        public string DetailID { get; set; }
+
+    }
+}
