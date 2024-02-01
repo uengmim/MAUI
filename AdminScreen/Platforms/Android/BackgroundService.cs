97675eb6093fb230a1f368d4ab6e2e724df2d763
채승민 <yalmipzzi@naver.com>
2024-02-01 09:44:43 +0900

    20240201 관리자

---
+++ AdminScreen/Platforms/Android/BackgroundService.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Views.InputMethods;
+using Com.Ttlock.BL.Sdk.Api;
+using Com.Ttlock.BL.Sdk.Callback;
+
+namespace AdminScreen
+{
+    [Service]
+    internal class BackgroundService : Service
+    {
+        public override IBinder OnBind(Intent intent)
+        {
+           return null;
+        }
+    }
+}
---
+++ AdminScreen/Platforms/Android/KeyboardHelper.cs
@@ -0,0 +1,23 @@
+﻿using Android.Content;
+using Android.Views.InputMethods;
+using Com.Ttlock.BL.Sdk.Api;
+using Com.Ttlock.BL.Sdk.Callback;
+
+namespace AdminScreen
+{
+    public static partial class KeyboardHelper
+    {
+        public static void HideKeyboard()
+        {
+            var context = Platform.AppContext;
+            var inputMethodManager = context.GetSystemService(Context.InputMethodService) as InputMethodManager;
+            if (inputMethodManager != null)
+            {
+                var activity = Platform.CurrentActivity;
+                var token = activity.CurrentFocus?.WindowToken;
+                inputMethodManager.HideSoftInputFromWindow(token, HideSoftInputFlags.None);
+                activity.Window.DecorView.ClearFocus();
+            }
+        }
+    }
+}
