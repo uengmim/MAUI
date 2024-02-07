33d16dda9be846939fc3e47d3a1fbd2adb2646bf
채승민 <yalmipzzi@naver.com>
2024-02-07 09:47:57 +0900

    20240207 괸리자

---
+++ AdminScreen/Views/LockChoicePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AdminScreen.ViewModel;
+using Mapsui.UI.Maui;
+using NetTopologySuite.GeometriesGraph;
+
+namespace AdminScreen.Views
+{
+    public partial class LockChoicePage : ContentPage
+    {
+
+        public LockChoicePage()
+        {
+            InitializeComponent();
+
+            LockChoiceViewModel lockChoiceViewModel = new LockChoiceViewModel();
+
+            this.BindingContext = lockChoiceViewModel;
+        }
+    }
+}
