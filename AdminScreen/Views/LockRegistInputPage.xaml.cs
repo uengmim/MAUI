5f4e9529d0a820a3339df7d189ae823d9e02da26
채승민 <yalmipzzi@naver.com>
2024-02-16 10:00:20 +0900

    20240216 관리자 등록

---
+++ AdminScreen/Views/LockRegistInputPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using AdminScreen.Models;
+using AdminScreen.ViewModels;
+using ShreDoc.ProxyModel;
+using ShreDoc.Utils;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+using XNSC.DD.EX;
+
+namespace AdminScreen.Views
+{
+    public partial class LockRegistInputPage : ContentPage
+    {
+        public LockRegistInputPage(LockInfomation lockInfo)
+        {
+            InitializeComponent();
+            LockRegistInputViewModel lockRegistDetailViewModel = new LockRegistInputViewModel();
+
+            lockRegistDetailViewModel.LockInfo = lockInfo;
+            lockRegistDetailViewModel.Mac = lockInfo.LockMac;
+
+            this.BindingContext = lockRegistDetailViewModel;
+
+        }
+        private string _mac = "";
+
+        private string _lockData = "";
+
+        public string LockData
+        {
+            get => _lockData;
+            set
+            {
+                _lockData = value;
+                OnPropertyChanged(nameof(LockData));
+            }
+        }
+
+        public string Mac
+        {
+            get => _mac;
+            set
+            {
+                _mac = value;
+                OnPropertyChanged(nameof(Mac));
+            }
+        }
+    }
+}
