8c0ea3874101760a9a985f5a62fb79c091b9c14a
채승민 <yalmipzzi@naver.com>
2024-02-20 09:23:02 +0900

    20240220 client 메인

---
+++ ClientScreen/Views/ClientMainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ClientScreen.ViewModel;
+
+namespace ClientScreen.Views
+{
+    public partial class ClientMainPage : ContentPage
+    {
+
+        public ClientMainPage(string DeptId, string Empno)
+        {
+            InitializeComponent();
+
+            MainViewModel mainViewModel = new MainViewModel();
+
+            mainViewModel.DEPTID = DeptId;
+            mainViewModel.EMPNO = Empno;
+
+
+
+            this.BindingContext = mainViewModel;
+
+        }
+
+        private async void Logout_Clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new ClientLoginPage());
+
+        }
+    }
+}
