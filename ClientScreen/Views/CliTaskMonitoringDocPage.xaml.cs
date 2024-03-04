ea65dc942d118ec873dd7b4079047ec0a76b80ac
채승민 <yalmipzzi@naver.com>
2024-03-04 09:32:52 +0900

    20240304 Client

---
+++ ClientScreen/Views/CliTaskMonitoringDocPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace ClientScreen.Views
+{
+    public partial class CliTaskMonitoringDocPage : ContentPage
+    {
+
+        public CliTaskMonitoringDocPage()
+        {
+            InitializeComponent();
+        }
+        public async void CliLockPictureClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CliDocLockPicturePage());
+        }
+
+        public async void CliCrushingReportClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CliDocCrushingReportPage());
+        }
+        public async void CliCrushingProofClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CliDocCrushingProofPage());
+        }
+    }
+}
