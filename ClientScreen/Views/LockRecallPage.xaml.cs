11b238f7610f036c1acbb4c9d904fbad249dcdb8
채승민 <yalmipzzi@naver.com>
2024-03-08 09:48:59 +0900

    20240308 client view

---
+++ ClientScreen/Views/LockRecallPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ClientScreen.Model;
+using ClientScreen.ViewModels;
+using ShreDoc.Utils;
+using System.Collections.ObjectModel;
+
+namespace ClientScreen.Views
+{
+
+    public partial class LockRecallPage : ContentPage
+    {
+        public LockRecallPage(string DeptId, string Wkpl, string Empno, string Refda2)
+        {
+            InitializeComponent();
+
+            LockRecallModel lockRecallModel = new LockRecallModel();
+            lockRecallModel.DEPTID = DeptId;
+            lockRecallModel.WKPL = Wkpl;
+            lockRecallModel.EMPNO = Empno;
+            lockRecallModel.REFDA2 = Refda2;
+
+
+
+
+
+            this.BindingContext = lockRecallModel;
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            loadingIndicator.IsRunning = true;
+            loadingIndicator.IsVisible = true;
+            base.OnAppearing();
+
+            if (BindingContext is LockRecallModel viewModel)
+            {
+                await viewModel.ExecuteMyCommand();
+            }
+            loadingIndicator.IsRunning = false;
+            loadingIndicator.IsVisible = false;
+        }
+    }
+
+}   
