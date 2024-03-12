831dc6c85bbd7687ea6ca0a962558c9b6d810ad2
채승민 <yalmipzzi@naver.com>
2024-03-12 09:32:31 +0900

    20240312 Worker

---
+++ WorkerScreen/Callback/RegistSearchScanCallback.cs
@@ -0,0 +1,25 @@
+﻿
+using WorkerScreen.ViewModel;
+
+namespace WorkerScreen
+{
+    /// <summary>
+    /// Lock Scan Callback
+    /// </summary>
+    public partial class RegistSearchScanCallback
+    {
+        /// <summary>
+        /// View 모델
+        /// </summary>
+        public QRRecogViewModel ViewModel { get; set; }
+
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public RegistSearchScanCallback(QRRecogViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+    }
+}
