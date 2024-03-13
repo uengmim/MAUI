d8a284d1cec77cd77e2b665193ccd2cc37145da2
채승민 <yalmipzzi@naver.com>
2024-03-13 09:24:14 +0900

    20240313 callback

---
+++ WorkerScreen/Platforms/Android/Callback/RegistControlLockCallback.Android.cs
@@ -0,0 +1,25 @@
+﻿using Com.Ttlock.BL.Sdk.Api;
+using Com.Ttlock.BL.Sdk.Callback;
+using Com.Ttlock.BL.Sdk.Entity;
+using XNSC.Net.NOKE;
+
+namespace WorkerScreen
+{
+    /// <summary>
+    /// UnLock Callback
+    /// </summary>
+    public partial class RegistControlLockCallback : Java.Lang.Object
+    {
+        public void OnFail(LockError p0)
+        {
+            Console.WriteLine($"{p0.Lockmac}, {p0.Description}");
+        }
+
+        public void onControlLockSuccess(ControlLockResult p0)
+        {
+            Console.WriteLine(p0);
+
+
+        }
+    }
+}
---
+++ WorkerScreen/Platforms/Android/Callback/RegistSearchScanCallback.Android.cs
@@ -0,0 +1,31 @@
+﻿using Com.Ttlock.BL.Sdk.Api;
+using Com.Ttlock.BL.Sdk.Callback;
+using Com.Ttlock.BL.Sdk.Entity;
+
+namespace WorkerScreen
+{
+    /// <summary>
+    /// Lock Scan Callback
+    /// </summary>
+    public partial class RegistSearchScanCallback : Java.Lang.Object, IScanLockCallback
+    {
+        public void OnFail(LockError p0)
+        {
+            Console.WriteLine($"{p0.Lockmac}, {p0.Description}");
+        }
+
+        public void OnScanLockSuccess(ExtendedBluetoothDevice p0)
+        {
+            //p0.Device
+            if (!ViewModel.LockInfoModel.Any(l => l.Device.Address == p0.Address))
+            {
+                var lockInfo = new Models.LockInfomation(p0);
+                lockInfo.SetLockInfo();
+
+                ViewModel.LockInfoModel.Add(lockInfo);
+            }
+
+            Console.WriteLine(p0.Address);
+        }
+    }
+}
