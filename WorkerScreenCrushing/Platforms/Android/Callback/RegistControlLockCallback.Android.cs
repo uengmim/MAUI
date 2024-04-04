93e98a0920da2b7bb36f4f7763a9797f39c841d9
채승민 <yalmipzzi@naver.com>
2024-04-04 09:46:42 +0900

    20240404 업데이트

---
+++ WorkerScreenCrushing/Platforms/Android/Callback/RegistControlLockCallback.Android.cs
@@ -0,0 +1,23 @@
+﻿using Com.Ttlock.BL.Sdk.Api;
+using Com.Ttlock.BL.Sdk.Callback;
+using Com.Ttlock.BL.Sdk.Entity;
+using XNSC.Net.NOKE;
+
+namespace WorkerScreenCrushing
+{
+    /// <summary>
+    /// UnLock Callback
+    /// </summary>
+    public partial class RegistControlLockCallback : Java.Lang.Object, IControlLockCallback
+    {
+        public void OnFail(LockError p0)
+        {
+            ParentVewModel.UnlockFail($"{p0.Lockmac}, {p0.Description}");
+        }
+
+        public void OnControlLockSuccess(ControlLockResult p0)
+        {
+            ParentVewModel.UnlockSuccess(p0.Battery, sierepData, siehisData, lkMstData);
+        }
+    }
+}
