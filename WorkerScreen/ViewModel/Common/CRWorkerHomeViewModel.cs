1fca3e4ccda196de663de18d37c0f2afaa9565ba
채승민 <yalmipzzi@naver.com>
2024-04-29 13:33:42 +0900

    파쇄 및 수거 작업자 화면

---
+++ WorkerScreen/ViewModel/Common/CRWorkerHomeViewModel.cs
@@ -0,0 +1,57 @@
+﻿using WorkerScreen.Models;
+using WorkerScreen.Views;
+using System.ComponentModel;
+using System.Windows.Input;
+using WorkerScreen.Views.CrushingWorker;
+
+namespace WorkerScreen.ViewModel.Common
+{
+    /// <summary>
+    /// 파쇄 작업자 홈페이지 화면입니다.
+    /// </summary>
+    public class CRWorkerHomeViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+        /// <summary>
+        /// 현재 로그인한 사용자 정보입니다.
+        /// </summary>
+        public LoginInfo LoginInfo
+        {
+            get => loginInfo;
+            set => loginInfo = value;
+        }
+        private LoginInfo loginInfo;
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// 파쇄등록 이동 Command 입니다.
+        /// </summary>
+        public ICommand CrushingDocCommand => new Command(OnCrushingDoc);
+        #endregion
+
+        #region 화면 이동
+        /// <summary>
+        /// 파쇄등록 화면 이동 버튼입니다.
+        /// </summary>
+        private async void OnCrushingDoc()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new CrushingDocument());
+        }
+        #endregion
+
+        #region CRWorkerHomeViewModel
+        public CRWorkerHomeViewModel()
+        {
+        }
+        #endregion
+    }
+}
---
+++ WorkerScreen/ViewModel/Common/PUWorkerHomeViewModel.cs
@@ -0,0 +1,90 @@
+﻿using WorkerScreen.Common;
+using WorkerScreen.Models;
+using WorkerScreen.Views;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+using WorkerScreen.Views.PickUpWorker;
+
+namespace WorkerScreen.ViewModel.Common
+{
+    /// <summary>
+    /// 메인 메뉴 화면입니다.
+    /// </summary>
+    public class PUWorkerHomeViewModel : INotifyPropertyChanged
+    {
+
+        #region Properties
+        /// <summary>
+        /// 현재 로그인한 사용자 정보입니다.
+        /// </summary>
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EMPNO { get; set; }
+        public string DEPTID { get; set; }
+        /// <summary>
+        /// 로그인한 사용자 정보입니다.
+        /// </summary>
+        public LoginInfo LoginInfo
+        {
+            get => loginInfo;
+            set => loginInfo = value;
+        }
+        private LoginInfo loginInfo;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// 홈페이지 이동 Command 입니다.
+        /// </summary>
+        public INavigation Navigation { get; set; }
+        public ICommand QRCodeCommand => new Command(OnQRCode);
+        public ICommand GetOnCommand => new Command(GetOnSecurity);
+        public ICommand GetOffCommand => new Command(GetOffSecurity);
+
+
+        #endregion
+
+        #region PUWorkerHomeViewModel
+
+        public PUWorkerHomeViewModel()
+        {
+        }
+        #endregion
+
+        #region 화면 이동
+        /// <summary>
+        /// 보안 문서 봉인 화면 이동입니다.
+        /// </summary>
+        private async void OnQRCode()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new QRCodePage(Name, PhoneNumber, EMPNO, DEPTID));
+        }
+        /// <summary>
+        /// 상차 화면 이동입니다.
+        /// </summary>
+        private async void GetOnSecurity()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new GetOnDocument());
+        }
+        /// <summary>
+        /// 하차 화면 이동입니다.
+        /// </summary>
+        private async void GetOffSecurity()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new GetOffDocument());
+        }
+
+
+        #endregion
+
+    }
+}
