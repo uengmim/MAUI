a4027961793c3cc2acd346947b7ac4fbc50fef3b
채승민 <yalmipzzi@naver.com>
2024-07-23 09:17:17 +0900

    20240723 작업이력 조회 페이지

--- AdminScreen/Views/HistoryDetailPage.xaml.cs
+++ AdminScreen/Views/HistoryDetailPage.xaml.cs
@@ -1,18 +1,9 @@
-
-using AdminScreen.Model;
-using AdminScreen.Models;
 using AdminScreen.ViewModels;
-using Kotlin.Reflect;
-using ShreDoc.ProxyModel;
-using ShreDoc.Utils;
-using XNSC.DD.EX;
 
 namespace AdminScreen.Views;
 
 public partial class HistoryDetailPage : ContentPage
 {
-
-    IGeolocation geolocation;
 
     public HistoryDetailPage(string LSN, string LKNM, string MAC, string LKTYP, string CONFNO)
     {
@@ -27,18 +18,61 @@ public partial class HistoryDetailPage : ContentPage
         this.BindingContext = historyDetailViewModel;
 
     }
+    // 중복 실행 방지를 위한 플래그
+    private bool isEventHandling = false;
     protected override async void OnAppearing()
     {
-        base.OnAppearing();
-
-        if (BindingContext is HistoryDetailViewModel viewModel)
+        // 중복 실행 방지 플래그 체크
+        if (isEventHandling)
         {
-            await viewModel.ExecuteMyCommand();
+            // 이미 실행 중이라면 더 이상 실행하지 않음
+            return;
+        }
+        try
+        {
+            isEventHandling = true;
+            base.OnAppearing();
+
+            if (BindingContext is HistoryDetailViewModel viewModel)
+            {
+                await viewModel.ExecuteMyCommand();
+            }
+        }
+        catch (Exception ex) {
+            await ShowCustomAlert("알림", ex.Message, "확인", "");
         }
     }
-    //파쇄 문서
-    public async void DocClicked(object sender, EventArgs e)
+
+    // 팝업 표시 상태를 나타내는 플래그
+    private bool isAlertShowing = false;
+
+    //팝업
+    private async Task ShowCustomAlert(string title, string message, string accept, string cancle)
     {
-        await Navigation.PushAsync(new TaskMonitoringDocPage());
+        // 이미 경고 팝업이 표시 중인 경우 추가적인 처리를 하지 않음
+        if (isAlertShowing)
+        {
+            return;
+        }
+
+        isAlertShowing = true; // 경고 팝업 표시 중임을 표시
+
+        // 팝업 애니메이션 비활성화
+        try
+        {
+            var alertPage = new CustomAlertPage(title, message, accept, cancle);
+            alertPage.Disappearing += (sender, e) => isAlertShowing = false;
+            await App.Current.MainPage.Navigation.PushModalAsync(alertPage, animated: false);
+        }
+        finally
+        {
+            isAlertShowing = true;
+        }
+
+    }
+    private void BackBtn_Clicked(object sender, EventArgs e)
+    {
+        //뒤로가기
+        Application.Current.MainPage.Navigation.PopAsync();
     }
 }
