17e229742aca9c8a2da6c884793f004f43effd58
채승민 <yalmipzzi@naver.com>
2024-07-30 10:50:29 +0900

    20240740 알림 문구 커스텀 및 슬라이드

---
+++ AdminScreen/ViewModels/MySlideViewModel.cs
@@ -0,0 +1,133 @@
+﻿using AdminScreen.Model;
+using AdminScreen.Views;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+using Mapsui.UI.Maui;
+using Microsoft.Maui.Layouts;
+
+namespace AdminScreen.ViewModel
+{
+    /// <summary>
+    /// 메인 화면
+    /// </summary>
+    public class MySlideView : AbsoluteLayout
+    {
+        public View MyTrackBar
+        {
+            get { return (View)GetValue(TrackBarProperty); }
+            set { SetValue(TrackBarProperty, value); }
+        }
+
+        public static readonly BindableProperty TrackBarProperty =
+            BindableProperty.Create(
+                "MyTrackBar", typeof(View), typeof(MySlideView),
+                defaultValue: default(View));
+
+        public View MyFillBar
+        {
+            get { return (View)GetValue(FillBarProperty); }
+            set { SetValue(FillBarProperty, value); }
+        }
+
+        public static readonly BindableProperty FillBarProperty =
+            BindableProperty.Create(
+                "MyFillBar", typeof(View), typeof(MySlideView),
+                defaultValue: default(View));
+
+        public View MyThumb
+        {
+            get { return (View)GetValue(ThumbProperty); }
+            set { SetValue(ThumbProperty, value); }
+        }
+
+        public static readonly BindableProperty ThumbProperty =
+       BindableProperty.Create(
+           "MyThumb", typeof(View), typeof(MySlideView),
+           defaultValue: default(View));
+
+
+        private PanGestureRecognizer _panGesture = new PanGestureRecognizer();
+        private View _gestureListener;
+        public MySlideView()
+        {
+            _panGesture.PanUpdated += OnPanGestureUpdated;
+            SizeChanged += OnSizeChanged;
+
+            _gestureListener = new ContentView { BackgroundColor = Colors.White, Opacity = 0.05 };
+            _gestureListener.GestureRecognizers.Add(_panGesture);
+        }
+
+        public event EventHandler SlideCompleted;
+
+        private const double _fadeEffect = 0.5;
+        private const uint _animLength = 50;
+        async void OnPanGestureUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            if (MyThumb == null || MyTrackBar == null || MyFillBar == null)
+                return;
+
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    await MyTrackBar.FadeTo(_fadeEffect, _animLength);
+                    break;
+
+                case GestureStatus.Running:
+
+                    var x = Math.Max(0, e.TotalX);
+                    var y = Math.Max(0, e.TotalY);
+                    if (x > (Width - MyThumb.Width))
+                        x = (Width - MyThumb.Width);
+                    if (y > (Height - MyThumb.Height)) y = (Height - MyThumb.Height);
+
+                    MyThumb.TranslationX = x;
+                    SetLayoutBounds((IView)MyFillBar, new Rect(0, 0, x + MyThumb.Width / 2, Height));
+                    break;
+
+                case GestureStatus.Completed:
+                    var posX = MyThumb.TranslationX;
+                    SetLayoutBounds((IView)MyFillBar, new Rect(0, 0, 0, this.Height));
+
+
+                    await Task.WhenAll(new Task[]{
+                    MyTrackBar.FadeTo(1, _animLength),
+                    MyThumb.TranslateTo(0, 0, _animLength * 2, Easing.CubicIn),
+                });
+
+                    if (posX >= (Width - MyThumb.Width - 10/* keep some margin for error*/))
+                        SlideCompleted?.Invoke(this, EventArgs.Empty);
+                    break;
+            }
+        }
+
+        void OnSizeChanged(object sender, EventArgs e)
+        {
+            if (Width == 0 || Height == 0)
+                return;
+            if (MyThumb == null || MyTrackBar == null || MyFillBar == null)
+                return;
+
+
+            Children.Clear();
+
+            SetLayoutFlags((IView)MyTrackBar, AbsoluteLayoutFlags.SizeProportional);
+            SetLayoutBounds((IView)MyTrackBar, new Rect(0, 0, 1, 1));
+            Children.Add(MyTrackBar);
+
+            SetLayoutFlags((IView)MyFillBar, AbsoluteLayoutFlags.None);
+            SetLayoutBounds((IView)MyFillBar, new Rect(0, 0, 0, this.Height));
+            Children.Add(MyFillBar);
+
+            SetLayoutFlags((IView)MyThumb, AbsoluteLayoutFlags.None);
+            SetLayoutBounds((IView)MyThumb, new Rect(0, 0, this.Width / 5, this.Height));
+            Children.Add(MyThumb);
+
+            SetLayoutFlags((IView)_gestureListener, AbsoluteLayoutFlags.SizeProportional);
+            SetLayoutBounds((IView)_gestureListener, new Rect(0, 0, 1, 1));
+            Children.Add(_gestureListener);
+        }
+    }
+}
---
+++ AdminScreen/Views/CustomAlertPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace AdminScreen.Views
+{
+    public partial class CustomAlertPage : ContentPage
+    {
+        private TaskCompletionSource<bool> userConfirmedTaskSource;
+
+        public ICommand AcceptCommand { get; private set; }
+        public ICommand CancelCommand { get; private set; }
+
+        public string CustomTitle { get; private set; }
+        public string Message { get; private set; }
+        public string Accept { get; private set; }
+        public string Cancel { get; private set; }
+
+        public Task<bool> UserConfirmedTask => userConfirmedTaskSource.Task;
+
+        public event EventHandler<bool> AcceptButtonClicked;
+        public event EventHandler<bool> CancelButtonClicked;
+
+        public CustomAlertPage(string title, string message, string accept, string cancel)
+        {
+            InitializeComponent();
+            CustomTitle = title;
+            Message = message;
+            Accept = accept;
+            Cancel = cancel;
+
+            userConfirmedTaskSource = new TaskCompletionSource<bool>();
+
+            AcceptCommand = new Command(async () =>
+            {
+                await CloseModalAsync();
+                AcceptButtonClicked?.Invoke(this, true);
+                userConfirmedTaskSource.TrySetResult(true);
+            });
+
+            CancelCommand = new Command(async () =>
+            {
+                CancelButtonClicked?.Invoke(this, false);
+
+                await CloseModalAsync();
+                userConfirmedTaskSource.TrySetResult(false);
+            });
+
+            BindingContext = this;
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            CloseModal();
+            return true; // 기본 동작을 막음
+        }
+
+        private async Task CloseModalAsync()
+        {
+            await Application.Current.MainPage.Navigation.PopModalAsync(animated: false);
+        }
+
+        private void CloseModal()
+        {
+            CloseModalAsync().Wait(); // 비동기 메서드를 동기적으로 호출하여 처리가 완료될 때까지 기다림
+        }
+    }
+}
